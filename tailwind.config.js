98e66255b93b41ed111fb800bc3af302ec686879
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-31 00:04:20 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    theme: {
+      fontFamily: {
+        sans: [
+          "Inter var, sans-serif",
+          { fontFeatureSettings: '"cv11", "ss01"' },
+        ],
+      },
+    },
+  }
